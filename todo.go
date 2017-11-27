81d02dd400494cca91b8a56337da6d7efaf8f102
Miguel Cabrera <syntacticsolutions@gmail.com>
2017-11-27 03:18:09 -0800

    enabled CORS

--- todo.go
+++ todo.go
@@ -30,6 +30,9 @@ func main() {
 
     e.Use(middleware.Logger())
     e.Use(middleware.Recover())
+    e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+        AllowOrigins: []string{"*"},
+    }))
 
     // Login route
     e.POST("/login", handlers.Login(db))
